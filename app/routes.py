5b4f25e2dc4f99cd0fe2eb3bf9b34c8d92518c73
SimonBr017 <simonbrard017@gmail.com>
2025-01-18 14:46:19 +0700

    basic general structure and home pages basic

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def home():
+    return render_template('home.html')
+
+@main.route('/about')
+def about():
+    return render_template('about.html')
+
+@main.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@main.route('/contact')
+def contact():
+    return render_template('contact.html')
